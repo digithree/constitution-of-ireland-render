5a56859317adb1e9506ebcdb1760c8b279a2c377
Simon Kenny <digithree@gmail.com>
2015-05-16 22:31:18 +0100

    Second debug change to /renderer

--- server.js
+++ server.js
@@ -69,10 +69,10 @@ var App = function() {
             } else {
                 //res.end(JSON.stringify(names));
                 console.log("Got all items from repo collection");
-                res.render('beard', JSON.stringify({
+                res.render('beard', JSON.stringify([{
                         direction: "",
                         sha: "ERROR"
-                    }));
+                    }]));
                 //JSON.stringify(items)
             }
         });
